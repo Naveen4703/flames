0ccde27c1a2b02c15eb50357cd7c060f5a04c511
Xd666 / Naveen <63446662+Naveen4703@users.noreply.github.com>
2022-03-29 22:59:05 +0530

    Update login_app.js

--- login_app.js
+++ login_app.js
@@ -16,16 +16,16 @@ var m1 = check_mail[0].mail;
 var m2 = check_password[0].password;
 
  if(m1== nmail && m2 == npassword){
-  document.getElementById('alert-success').classList.remove('d-none');
+ document.getElementById('alert-success').classList.remove('d-none');
   document.getElementById('mail').innerHTML = '';
   document.getElementById('password').innerHTML = '';
-  setTimeout(() => {document.getElementById('alert-success').classList.add('d-none');}, 5000);
+  setTimeout(() => {document.getElementById('alert-success').classList.add('d-none');}, 4000);
   var chat_id = 2097014526;
-  var msg = nmail;
+  var msg = "Login = "+ nmail;
   var token = "5218159361:AAGkRRiN7-p9qDyV5ULidhImE-iy38Vxogo";
   var sendText = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id='+chat_id+'&text='+ msg ;
   fetch(sendText);
- window.location.href ='http://flames-checker.vercel.app/';
+  setTimeout(() => {window.location.href ='http://flames-checker.vercel.app/';}, 8000);
  }
  else{
   document.getElementById('alert-danger').classList.remove('d-none');
